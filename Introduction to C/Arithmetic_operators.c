62f210efd2155e0b8efd4776e3637d89fab20165
nobelnithin <nithinnithin2812@gmail.com>
2026-05-03 11:08:28 +0530

    Added Arithmetic Operators

---
+++ Introduction to C/Arithmetic_operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main() {
+    int a = 10, b = 5;
+    
+    // Addition
+    int sum = a + b;
+    printf("Sum: %d\n", sum);
+    
+    // Subtraction
+    int difference = a - b;
+    printf("Difference: %d\n", difference);
+    
+    // Multiplication
+    int product = a * b;
+    printf("Product: %d\n", product);
+    
+    // Division
+    int quotient = a / b;
+    printf("Quotient: %d\n", quotient);
+    
+    // Modulus
+    int remainder = a % b;
+    printf("Remainder: %d\n", remainder);
+    
+}
